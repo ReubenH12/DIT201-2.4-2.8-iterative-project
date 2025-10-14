d0587b0a1349d61ba4e073cc64f8767752ce1680
ReubenH12 <77756527+ReubenH12@users.noreply.github.com>
2025-10-15 11:51:02 +1300

    Upload v7 (index_v7)
    This is the upload of the code for the final completed version of the website, after the end of Stage 3. It adds responsive features and cleans up the last bits. It is the seventh total version of the website (index_v7), and the Projectspace equivalent is index_v7.html, style_v7.css, and script_v4.js

--- js/script.js
+++ js/script.js
@@ -7,18 +7,39 @@ const pageNames = {
     "Contact": "contact"
 }
 
-const lineExtension = 20; // Distance by which to extend the nav indicator line past the edge of the text above
+const defaultLineExtension = 20; // This is the normal line extension (referenced below) that is used for the non-hamburger menu
 
-// Define the list of <li> navigation elements and the name of the current page
-let navButtons = document.querySelectorAll("nav ul li");
+// This section runs the hamburger menu so that it can be toggled and the indicator line redrawn
+
+navMenuOn = false
+hamburgerButton = document.getElementById("hamburger");
+navMenu = document.getElementById("menu");
+
+hamburgerButton.addEventListener("click", function() {
+    if (navMenuOn == false) {
+        navMenuOn = true
+        drawIndicatorLine(10);
+    } else {
+        navMenuOn = false
+    }
+
+    navMenu.classList.toggle("opened")
+});
+
+
+
+// This section makes the indicator line underneath the menu items in the navigation
+
+// Define the list of <a> navigation elements and the name of the current page
+let navButtons = document.querySelectorAll("nav ul li a");
 let currentPage = document.querySelector("body").id;
-// Also define the <li> of the current page as a global variable here
+// Also define the <a> of the current page as a global variable here
 let currentButton;
 
-// Check through the <li> elements until one of them matches the current page
+// Check through the <a> elements until one of them matches the current page
 for (let i = 0; i < navButtons.length; i++) {
     if (pageNames[navButtons[i].children[0].innerHTML] == currentPage) {
-        // Re-set current button to this <li>
+        // Re-set current button to this <a>
         currentButton = navButtons[i];
     }
 }
@@ -28,15 +49,23 @@ let pageIndicator = document.createElement("div");
 pageIndicator.id = "page-indicator";
 currentButton.appendChild(pageIndicator);
 
-// The line to then resize the line is run twice. Once as soon as the page loads, in order to have the line be close to the correct size ASAP, and another to make it the perfect size after the Inter font loads, which is a slightly different width to the initial. The children[0] part chooses specifically the paragraph element in the li, rather than the li itself. 
-pageIndicator.style.width = `${currentButton.children[0].offsetWidth + lineExtension}px`;
+// Splitting this part into a function means it can be run again when the line needs to be redrawn, such as when the hamburger menu is opened. The parameter is how much wider than the text above the line should be, which varies depending on whether it's in the normal menu or the hamburger menu. 
+
+function drawIndicatorLine(extention) {
+    pageIndicator.style.width = `${currentButton.children[0].offsetWidth + extention}px`; // Using children[0] part chooses specifically the paragraph element in the li, rather than the li itself.
+    pageIndicator.style.translate = `-${extention/2}px` // Offsets the horizontal position of the line by half the total width change}
+}
+
+// The line function is then run twice. Once as soon as the page loads, in order to have the line be close to the correct size ASAP, and another to make it the perfect size after the Inter font loads, which is a slightly different width to the initial.  
+
+drawIndicatorLine(defaultLineExtension);
 document.fonts.ready.then(() => {
-    pageIndicator.style.width = `${currentButton.children[0].offsetWidth + lineExtension}px`;
+    drawIndicatorLine(defaultLineExtension);
 });
 
-pageIndicator.style.translate = `-${lineExtension/2}px` // Offsets the horizontal position of the line by half the total width change
 
 
+// This section loads in all the background-image properties. Explained in diary. 
 
 // Get every element in the document with the "image" class
 let images = document.getElementsByClassName("image");
@@ -49,67 +78,82 @@ for (let i = 0; i < images.length; i++) {
 }
 
 
+// This section resizes the elements in the navigation to ensure the middle element is always centered
 
 // Get the two left and right boxes in the footer
 let linksBox = document.getElementById("external-links");
 let logoBox = document.getElementById("footer-logo");
 
+// Set up variables for the larger one and the smaller one
+let smallBox;
+let bigBox;
+
+// Set the smallBox and bigBox variables
+if (linksBox.offsetWidth > logoBox.offsetWidth) {
+    smallBox = logoBox;
+    bigBox = linksBox;
+} else {
+    smallBox = linksBox;
+    bigBox = logoBox;
+}
+
 // When fonts are loaded, set the width of the left box to be the same as the right to center the middle box
 document.fonts.ready.then(() => {
-    logoBox.style.width = `${linksBox.offsetWidth}px`;
+    smallBox.style.width = `${bigBox.offsetWidth}px`;
 });
 
 
-// This section of code manages the movement of the indicator line upon hover on the other navigation items
-// The entire section is only run when the fonts load, as that changes some widths and alignments
-document.fonts.ready.then(() => {
 
-    // Animation settings
-    const options = {
-        duration: 500,
-        fill: "forwards",
-        easing: "ease-in-out",
-    };
+// This section manages the movement of the indicator line upon hover on the other navigation items. It only runs if the normal, horizontal nav exisits (on desktops), as it would be useless on phones with the vertical nav menu. 
 
-    // Define the navigation items. This chooses the paragraph, not the li
-    let menuItems = document.querySelectorAll("#page-links li p")
-    // The "boundingClientRect" returns data for where on the page the element is
-    let lineRect = pageIndicator.getBoundingClientRect();
+if (window.innerWidth > 1200) {
 
-    for (let i = 0; i < menuItems.length; i++) {
-        // Define the current iteration's element and its location with boundingClientRect
-        let menuItem = menuItems[i];
-        let menuItemRect = menuItem.getBoundingClientRect();
-        // Calculate the difference between the current line location and the required location for this nav item
-        let offsetX = menuItemRect.x - lineRect.x - lineExtension/2;
+    // The entire section is only run when the fonts load, as that changes some widths and alignments.
+    document.fonts.ready.then(() => {
 
-        // Transform property for the final location of the line (after movement)
-        let enterFrames = [
-            {transform: `translateX(${offsetX}px)`, width: `${menuItemRect.width + lineExtension}px`},
-        ]
+        // Animation settings
+        const options = {
+            duration: 500,
+            fill: "forwards",
+            easing: "ease-in-out",
+        };
 
-        // Add an event listener to animate the line with the above parameters upon hover on the nav item. While we generally work with the paragraph element inside the li, this time it needs to be the li, as the user should be able to hover over the entire width of the element. 
-        menuItem.parentElement.addEventListener("mouseenter", function() {
-            pageIndicator.animate(enterFrames, options);
-            // console.log("Run mouseenter")
-            // console.log(menuItem)
-            // console.log(`${menuItemRect.x} - ${lineRect.x} - ${lineExtension/2} = ${offsetX}`)
-            // console.log(enterFrames)
-        });
+        // Define the navigation items. This chooses the paragraph, not the li
+        let menuItems = document.querySelectorAll("#page-links li p")
+        // The "boundingClientRect" returns data for where on the page the element is
+        let lineRect = pageIndicator.getBoundingClientRect();
+
+        for (let i = 0; i < menuItems.length; i++) {
+            // Define the current iteration's element and its location with boundingClientRect
+            let menuItem = menuItems[i];
+            let menuItemRect = menuItem.getBoundingClientRect();
+            // Calculate the difference between the current line location and the required location for this nav item
+            let offsetX = menuItemRect.x - lineRect.x - defaultLineExtension/2;
+
+            // Transform property for the final location of the line (after movement)
+            let enterFrames = [
+                {transform: `translateX(${offsetX}px)`, width: `${menuItemRect.width + defaultLineExtension}px`},
+            ]
+
+            // Add an event listener to animate the line with the above parameters upon hover on the nav item. While we generally work with the paragraph element inside the li, this time it needs to be the li, as the user should be able to hover over the entire width of the element. 
+            menuItem.parentElement.addEventListener("mouseenter", function() {
+                pageIndicator.animate(enterFrames, options);
+            });
 
 
-        // Get location of current page nav item
-        let currentItemRect = currentButton.children[0].getBoundingClientRect();
+            // Get location of current page nav item
+            let currentItemRect = currentButton.children[0].getBoundingClientRect();
 
-        // This resets the line back to where it started, under the current page name
-        let leaveFrames = [
-            {transform: `translateX(0px)`, width: `${currentItemRect.width + lineExtension}px`},
-        ]
+            // This resets the line back to where it started, under the current page name
+            let leaveFrames = [
+                {transform: `translateX(0px)`, width: `${currentItemRect.width + defaultLineExtension}px`},
+            ]
 
-        // Add another event listenter to return the line to the current page name when when the user stops hovering
-        menuItem.parentElement.addEventListener("mouseleave", function() {
-            pageIndicator.animate(leaveFrames, options)
-        });
-    }
+            // Add another event listenter to return the line to the current page name when when the user stops hovering
+            menuItem.parentElement.addEventListener("mouseleave", function() {
+                pageIndicator.animate(leaveFrames, options)
+            });
+        }
 
-});
+    });
+}
